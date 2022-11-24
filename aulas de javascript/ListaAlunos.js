ddf39e865bfa5a8123fbac0f90b6519b9ceae841
Programmer Juninho <110572176+Evandrobueno123@users.noreply.github.com>
2022-11-23 21:01:29 -0300

    23/11/2022

---
+++ aulas de javascript/ListaAlunos.js
@@ -0,0 +1,24 @@
+let aluno = 'Jamir'
+
+switch(aluno){
+    case 'Leticia':
+        console.log('Leticia tem 17 anos')
+        break
+    case 'yuri':
+        console.log('Yuri tem 16 anos ')
+        break
+    case 'Maria Eduarda':
+        console.log('Maria Eduarda')
+        break
+    case 'Jamir':
+        console.log('Jamir tem 20 anos')
+        break
+    case 'Maryane':
+        console.log('Maryane tem 15')
+    case 'Talita':
+        console.log('Talita tem 21')
+    case 'kauan':
+        console.log('Kauan tem 16')
+    case 'Emily':
+        console.log('Emily tem 19 anos')
+}
---
+++ aulas de javascript/contador.js
@@ -0,0 +1,8 @@
+let contadorString=''
+let i = 0 
+
+do {
+    contadorString = contadorString + i 
+    i ++
+} while (i < 5 )
+    console.log(contadorString)
