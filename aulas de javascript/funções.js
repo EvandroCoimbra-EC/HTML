34560f31b0e7c4d7943048929a59b632a68f931d
Programmer Juninho <110572176+Evandrobueno123@users.noreply.github.com>
2022-11-26 13:00:35 -0300

    26/11/2022

---
+++ aulas de javascript/funções.js
@@ -0,0 +1,8 @@
+ var numero1 = prompt("escreva o primero número ")
+ var numero2 = prompt("escreva o segundo número")
+
+ function divisão   (){
+    var divisão1 = parseInt (numero1) / parseInt (numero2)
+    console.log(divisão1)
+ }
+ divisão()
